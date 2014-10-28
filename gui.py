4080aed358f003ea99962dbfac4ceffc020c6591
dcelik <deniz.celik@students.olin.edu>
2014-10-28 14:00:39 -0400

    Modified to only edit binary threshold

--- gui.py
+++ gui.py
@@ -13,11 +13,8 @@ cap = cv2.VideoCapture(1)
 # Create a black image, a window
 cv2.namedWindow('image')
 # create trackbars for color change
-cv2.createTrackbar('Value1','image',11,11,nothing)
-cv2.createTrackbar('Value2','image',2,2,nothing)
-cv2.createTrackbar('Value3','image',255,255,nothing)
-cv2.createTrackbar('Value4','image',0,255,nothing)
-cv2.createTrackbar('Value5','image',255,255,nothing)
+cv2.createTrackbar('Value1','image',255,255,nothing)
+cv2.createTrackbar('Value2','image',0,255,nothing)
 
 # create switch for ON/OFF functionality
 switch = '0 : OFF \n1 : ON'
@@ -32,31 +29,22 @@ while(1):
     # get current positions of four trackbars
     Value1 = cv2.getTrackbarPos('Value1','image')
     Value2 = cv2.getTrackbarPos('Value2','image')
-    Value3 = cv2.getTrackbarPos('Value3','image')
-    Value4 = cv2.getTrackbarPos('Value4','image')
-    Value5 = cv2.getTrackbarPos('Value5','image')
     s = cv2.getTrackbarPos(switch,'image')
 
     if s == 0:
-        v1 = 11
-        v2 = 2
-        v3 = 255
-        v4 = 65
-        v5 = 8
+        v1 = 127
+        v2 = 255
     else:
         v1 = Value1
         v2 = Value2
-        v3 = Value3
-        v4 = Value4
-        v5 = Value5
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray,v4,v5,cv2.THRESH_BINARY)
-    th2 = cv2.adaptiveThreshold(thresh,v3,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-                cv2.THRESH_BINARY,v1,v2)
+    ret, thresh = cv2.threshold(gray,v1,v2,cv2.THRESH_BINARY)
+    th2 = cv2.adaptiveThreshold(thresh,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                cv2.THRESH_BINARY,11,2)
     # Display the resulting frame
-    cv2.imshow('image',th2)
+    cv2.imshow('image',thresh)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
