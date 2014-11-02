5dc20d00ae7a600b4f3bc2f83fb1d961651677d1
Deniz Celik <deniz.celik@students.olin.edu>
2014-11-02 14:49:02 -0500

    removed drawing modes

--- drawingTest.py
+++ drawingTest.py
@@ -2,12 +2,11 @@ import cv2
 import numpy as np
 
 drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle. Press 'm' to toggle to curve
 ix,iy = -1,-1
 
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing,mode
+    global ix,iy,drawing
 
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
@@ -15,17 +14,11 @@ def draw_circle(event,x,y,flags,param):
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
-            if mode == True:
-                cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-            else:
-                cv2.circle(img,(x,y),5,(0,0,255),-1)
+            cv2.circle(img,(x,y),2,(0,0,255),-1)
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
-        if mode == True:
-            cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-        else:
-            cv2.circle(img,(x,y),5,(0,0,255),-1)
+        cv2.circle(img,(x,y),2,(0,0,255),-1)
 
 img = cv2.imread('denizhand.png')
 cv2.namedWindow('image')
@@ -34,8 +27,6 @@ cv2.setMouseCallback('image',draw_circle)
 while(1):
     cv2.imshow('image',img)
     k = cv2.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
     if k == ord('q'):
         break
     elif k == 27:
