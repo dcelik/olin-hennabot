9de18da2d24ceff11b5bdede154e677e27c8f209
dcelik <deniz.celik@students.olin.edu>
2014-11-20 22:44:40 -0500

    Added serial test code

--- Arduino Code/SERIALTEST/SERIALTEST.ino
+++ Arduino Code/SERIALTEST/SERIALTEST.ino
@@ -1,14 +1,21 @@
 const int ledPin =  13; 
-
+int incbyte = 0;
 void setup() {
   pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  incbyte = Serial.read();
+  Serial.print(incbyte);
+  if(incbyte==48)
+  {
     digitalWrite(ledPin, HIGH);
     delay(100);
     digitalWrite(ledPin, LOW);
     delay(100);
+  }
+  delay(10);
 }
 
